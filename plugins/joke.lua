bcf17949567094148ea963e42b5a80d744d01440
kiava <info@Kiava.ir>
2016-05-09 21:06:13 +0430

    joke plugins by kiava

---
+++ plugins/joke.lua
@@ -0,0 +1,16 @@
+local database = 'http://vip.opload.ir/vipdl/94/11/amirhmz/'
+local function run(msg)
+	local res = http.request(database.."joke.db")
+	local joke = res:split(",") 
+	return joke[math.random(#joke)]
+end
+--Joke Plugin v2.0 By @kiavabots
+return {
+	description = "1000 Persian Joke",
+	usage = "#جوک : send random joke",
+	patterns = {
+		"^[/#!]جوک$",
+		"^(جوک)$"
+		},
+	run = run
+}
