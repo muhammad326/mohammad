f7e07b238ac893cbde150f4f8e687a9ac781c0f1
kiava <info@Kiava.ir>
2016-05-09 20:06:48 +0430

    اوردن شما به گروه برای پشتیبانی

---
+++ plugins/support.lua
@@ -0,0 +1,36 @@
+do
+
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg, matches)
+  local user = 174997972
+
+  if matches[1] == "support" then
+    user = 'user#id'..user
+  end
+
+  -- The message must come from a chat group
+  if msg.to.type == 'chat' then
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "سازنده ربات وارد گروه شد"
+  else 
+    return 'This isnt a chat group!'
+  end
+
+end
+
+return {
+  description = "support", 
+  patterns = {
+    "^[!/](support)$"
+  }, 
+  run = run 
+}
+
+end
+--Copyright; @kiava_ir
+--کپی بدون ذکر منبع حرام است
