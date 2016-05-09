a45bb03a9d89c10000911a6c082b4f1a3140d43e
kiava <info@Kiava.ir>
2016-05-09 19:52:46 +0430

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,27 @@
+do
+
+function run(msg, matches)
+  return 'UnKnOwNsBoT v4'.. VERSION .. [[ 
+ 
+  Developer : @kiava_ir , @Xx_TiKTaK_xX
+  
+  Channel : @UnKnOwNsBoTCH
+  
+  Open git : https://github.com/kiavacompany/UnKnOwNsBoT.git
+  
+
+  
+  All rights reserved.
+  __________________]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^!version$"
+  }, 
+  run = run 
+}
+
+end
