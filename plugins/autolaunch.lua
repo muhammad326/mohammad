506c78eb3cb53815646480d17e5952b7243064f1
kiava <info@Kiava.ir>
2016-05-09 18:10:31 +0430

    auto luanch

---
+++ plugins/autolaunch.lua
@@ -0,0 +1,18 @@
+[Forwarded from FuckSoN🌐HACK🌍]
+function run(msg, matches)
+if not is_sudo(msg) then
+return 
+end
+text = io.popen(" ./launch.sh "):read('*all')
+  return text
+end
+return {
+  patterns = {
+    '^[#/!]launch$'
+  },
+  run = run,
+  moderated = true
+}
+
+
+-- مرجع کامل پلاگین @UnKnOwNsBoTCH --
