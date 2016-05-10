50bec2b2060e474171f97ae8af2d8c6e61dfd291
kiava <info@Kiava.ir>
2016-05-10 20:32:31 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -239,7 +239,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {174997972,152043560,158576539,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[UnKnOwNsBoT v4
 An advanced administration bot based on TG-CLI written in Lua
